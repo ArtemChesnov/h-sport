86f0a1eb9d55cdfbe74c5591729db9a685c00cba
Артём Чеснов <88346227+ArtemChesnov@users.noreply.github.com>
2026-05-18 23:33:45 +0300

    fix about

--- app/(shop)/about/about-page-client.tsx
+++ app/(shop)/about/about-page-client.tsx
@@ -106,20 +106,20 @@ export function AboutPageClient() {
             Мы создаём стиль, который двигается вместе с тобой.
             <br />
             Наш бренд родился из любви к активной жизни и стремления объединить эстетику, комфорт и
-            свободу движения.
+            свободу движения
           </p>
           <p
             ref={block2Ref}
             className="max-[576px]:w-full max-[576px]:max-w-full max-[576px]:ml-0 min-[577px]:ml-8 min-[768px]:ml-16 min-[1024px]:ml-24 min-[1920px]:ml-40 min-[577px]:max-w-[400px] min-[768px]:max-w-[520px] min-[1024px]:max-w-[620px] min-[1280px]:max-w-[700px] min-[1920px]:max-w-[790px] text-[16px] max-[576px]:text-[14px] min-[577px]:text-[18px] min-[768px]:text-[22px] min-[1920px]:text-[26px] leading-[130%]"
           >
             Каждая вещь H-Sport — это результат точного баланса между трендом и функциональностью:
-            от зала до улиц, от утренней пробежки до вечернего расслабления.
+            от зала до улиц, от утренней пробежки до вечернего расслабления
           </p>
           <p
             ref={block3Ref}
             className="max-[576px]:w-full max-[576px]:max-w-full max-[576px]:ml-0 min-[577px]:ml-12 min-[768px]:ml-24 min-[1024px]:ml-40 min-[1280px]:ml-100 min-[1920px]:ml-158.75 min-[577px]:max-w-[400px] min-[768px]:max-w-[520px] min-[1024px]:max-w-[620px] min-[1280px]:max-w-[700px] min-[1920px]:max-w-[790px] text-[16px] max-[576px]:text-[14px] min-[577px]:text-[18px] min-[768px]:text-[22px] min-[1920px]:text-[26px] leading-[130%]"
           >
-            Мы верим, что спорт — это не про идеал, а про силу быть собой.
+            Мы верим, что спорт — это не про идеал, а про силу быть собой
           </p>
         </div>
 
